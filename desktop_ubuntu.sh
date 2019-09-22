5dc393b0cb269dfc379a1bf5fbfa355fd31c189f
infosecirvin <il.tmri@pm.me>
2019-09-21 22:00:34 -0700

    modified:   desktop_ubuntu.sh

--- desktop_ubuntu.sh
+++ desktop_ubuntu.sh
@@ -3,13 +3,13 @@
 #Assuming you are Root:
 
 apt update && apt dist-upgrade -y
-apt install --no-install-recommends ubuntu-mate-core ubuntu-mate-desktop -y
-apt install mate-core mate-desktop-environment mate-notification-daemon xrdp -y
-apt install apt-transport-https curl -y
+#apt install --no-install-recommends ubuntu-mate-core ubuntu-mate-desktop -y
+#apt install mate-core mate-desktop-environment mate-notification-daemon xrdp -y
+apt install apt-transport-https curl xfce4 xrdp -y
 curl -s https://brave-browser-apt-release.s3.brave.com/brave-core.asc | sudo apt-key --keyring /etc/apt/trusted.gpg.d/brave-browser-release.gpg add -
 source /etc/os-release
 echo "deb [arch=amd64] https://brave-browser-apt-release.s3.brave.com/ $UBUNTU_CODENAME main" | sudo tee /etc/apt/sources.list.d/brave-browser-release-${UBUNTU_CODENAME}.list
-apt update && apt install brave-browser openvpn -y
+apt update && apt install brave-browser wireshark openvpn -y
 
 #users
 clear
@@ -23,6 +23,6 @@ do
         echo baccc$counter:baccc123 | chpasswd
         ((counter++))
 done
-sed -i '7i\echo mate-session >~/.xsession\' /etc/xrdp/startwm.sh
+sed -i '7i\echo xfce4-session >~/.xsession\' /etc/xrdp/startwm.sh
 service xrdp restart
 echo "Ready for Work"
