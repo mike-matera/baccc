05a5a29dc5d32d071f0889e91625a4e561dc0cbf
infosecirvin <il.tmri@pm.me>
2019-09-23 17:16:45 -0700

    renamed:    desktop_ubuntu.sh -> desktop.sh

--- desktop.sh
+++ desktop.sh
@@ -1,23 +1,21 @@
 #!/bin/bash
 #This script will prepare a Cloud VM (Digital Ocean / Google Cloud) for use with 100 users via RDP with Brave, and OpenVPN installed. 
 #Assuming you are Root:
-
 apt update && apt dist-upgrade -y
-#apt install --no-install-recommends ubuntu-mate-core ubuntu-mate-desktop -y
-#apt install mate-core mate-desktop-environment mate-notification-daemon xrdp -y
 apt install apt-transport-https curl xfce4 xrdp -y
 curl -s https://brave-browser-apt-release.s3.brave.com/brave-core.asc | sudo apt-key --keyring /etc/apt/trusted.gpg.d/brave-browser-release.gpg add -
 source /etc/os-release
 echo "deb [arch=amd64] https://brave-browser-apt-release.s3.brave.com/ $UBUNTU_CODENAME main" | sudo tee /etc/apt/sources.list.d/brave-browser-release-${UBUNTU_CODENAME}.list
-apt update && apt install brave-browser wireshark openvpn -y
+apt update && apt install brave-browser zip unzip openjdk-11-jre openvpn wireshark -y
 
-#users
+
+#User Creation
 clear
 echo "Creating Users"
 counter=1
 while  [ $counter -le 100 ]
 do
-        useradd -m baccc$counter
+        useradd -s /bin/bash -m baccc$counter
         usermod -aG sudo baccc$counter
         usermod -aG admin baccc$counter
         echo baccc$counter:baccc123 | chpasswd
