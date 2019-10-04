78a999b1cc5673c6016d1abed2e25c96223330eb
infosecirvin <il.tmri@pm.me>
2019-10-03 18:53:33 -0700

    new file:   100519/example1 	renamed:    desktop.sh -> vmsetup/desktop.sh

--- vmsetup/desktop.sh
+++ vmsetup/desktop.sh
@@ -6,7 +6,7 @@ apt install apt-transport-https curl xfce4 xrdp -y
 curl -s https://brave-browser-apt-release.s3.brave.com/brave-core.asc | sudo apt-key --keyring /etc/apt/trusted.gpg.d/brave-browser-release.gpg add -
 source /etc/os-release
 echo "deb [arch=amd64] https://brave-browser-apt-release.s3.brave.com/ $UBUNTU_CODENAME main" | sudo tee /etc/apt/sources.list.d/brave-browser-release-${UBUNTU_CODENAME}.list
-apt update && apt install firefox brave-browser zip unzip openjdk-11-jre icoutils openvpn wireshark -y
+apt update && apt install firefox gcc gdb docker.io brave-browser zip unzip openjdk-11-jre icoutils openvpn wireshark -y
 
 #User Creation
 clear
