18ddf1fb2c223cd61be74050667655fe576919c2
infosecirvin <il.tmri@pm.me>
2019-10-12 22:42:10 -0700

    modified:   kali.sh

--- vmsetup/kali.sh
+++ vmsetup/kali.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 #This script will prepare a Cloud VM (Digital Ocean) into a Kali Machine accessible via RDP.
 #Assuming you are Root:
-
 apt update
 apt install dirmngr -y
+rm -rf /etc/apt/sources.list
 echo "deb http://http.kali.org/kali kali-rolling main non-free contrib" >> /etc/apt/sources.list
 apt-key adv --keyserver hkp://keys.gnupg.net --recv-keys 7D8D0BF6
 apt update && apt dist-upgrade -y
